cc4c9a47482b047d1680d52fa9bd0a82d9826ac6
abotsi <isaac.abotsi@gmail.com>
2015-06-23 17:54:41 +0200

    Fifth

--- p3exblock/p3exblock.py
+++ p3exblock/p3exblock.py
@@ -1,10 +1,12 @@
+# coding: utf-8
 """TO-DO: Write a description of what this XBlock is."""
 
 import pkg_resources
 import logging
+from random import sample, shuffle
 
 from xblock.core import XBlock
-from xblock.fields import Scope, Integer, String, List
+from xblock.fields import Scope, Integer, String, List, Dict
 from xblock.fragment import Fragment
 
 from mako.template import Template
@@ -12,58 +14,47 @@ from mako.template import Template
 from models import Question, Answer
 
 APP_PATH = "/home/abotsi/Documents/stage_p3e/fun/edxwork/p3exblock/p3exblock/"
+# APP_PATH = "/home/pilou/xblock-sdk/p3exblock/p3exblock/"
 
 class P3eXBlock(XBlock):
     """
     TO-DO: document what your XBlock does.
     """
 
-    questions = List(
-        default=[], scope=Scope.user_state_summary,
+    dict_questions = Dict(
+        default={}, scope=Scope.user_state_summary,
         help="The list of all questions",
+    )
+    max_id_question = Integer(
+        default=0, scope=Scope.user_state,
+        help="The phase currently running",
     )
     current_phase = Integer(
         default=1, scope=Scope.user_state,
         help="The phase currently running",
     )
-    r1 = String(
-        default="", scope=Scope.user_state,
-        help="The first answer of the phase 1",
+    phase1_question_indexes = List(
+        default=[], scope=Scope.user_state,
+        help="The ids of the 3 questions this student answered in phase 1",
     )
-    r2 = String(
-        default="", scope=Scope.user_state,
-        help="The second answer of the phase 1",
+    phase2_question_index = Integer(
+        default=[], scope=Scope.user_state,
+        help="The id of the question this student asked in phase 1",
     )
-    r3 = String(
-        default="", scope=Scope.user_state,
-        help="The third answer of the phase 1",
-    )
-
-    q = String(
-        default="", scope=Scope.user_state,
-        help="The question proposed by the student",
-    )
-    r = String(
-        default="", scope=Scope.user_state,
-        help="The answer proposed by the student",
+    phase3_answer_indexes = List(
+        default=[], scope=Scope.user_state,
+        help="The ids of the 9 answers this student corrected in phase 1",
     )
     
     def studio_view(self, context):
         pass
 
     def student_view(self, context=None):
-        # print
-        # print "Debut"
-        # print self.questions
-        # self.questions.append({'1': 'prout', '2':"pet"})
-        # self.questions.append({'message': "coucou"})
-        # print self.questions
-        # print "Fin"
-        # print
+        if len(self.dict_questions)<10:
+            for i in range(10):
+                self.add_question("question bidon n"+str(i), "reponse bidon n"+str(i), p_is_prof=True)
+                self.add_question("question eleve bidon n"+str(i), "reponse bidon n"+str(i))
 
-        self.current_phase = 2
-
-        
         data = []
         if (self.current_phase == 1):
             data = self.get_data_phase1()
@@ -73,16 +64,21 @@ class P3eXBlock(XBlock):
         return self.load_current_phase(data)
 
 
-    def load_current_phase(self, data):
-        """Handy helper for loading mako template."""
-        f = APP_PATH+"templates/phase"+ str(self.current_phase) +".html"
-        html = Template(filename=f, input_encoding='utf-8').render(data=data)
+    def load_current_phase(self, p_data):
+        """Loading the whole XBlock fragment"""
 
-        frag = Fragment(html)
+        frag = Fragment(self.get_current_html(p_data))
         frag.add_css(self.resource_string("static/css/p3exblock.css"))
         frag.add_javascript(self.resource_string("static/js/src/p3exblock.js"))
         frag.initialize_js('P3eXBlock')
         return frag
+
+    def get_current_html(self, p_data=[]):
+        """Handy helper for loading mako template."""
+        f = APP_PATH+"templates/phase"+ str(self.current_phase) +".html"
+        # f = self.runtime.local_resource_url(self, "public/templates/phase"+ str(self.current_phase) +".html")
+        html = Template(filename=f, input_encoding='utf-8').render(data=p_data)
+        return html
 
     def resource_string(self, path):
         """Handy helper for getting resources from our kit."""
@@ -92,13 +88,29 @@ class P3eXBlock(XBlock):
 
     def get_data_phase1(self, context=None):
         """
-        The first phase of the P3E
+        Selecting 3 random questions for the first phase
         """
-        q1 = "Lorem ipsum dolor sit amet ?"
-        q2 = "Praesent pulvinar in suscipit ?"
-        q3 = "Mauris at risus in ipsum ?"
+        
+        lst_txt = []        
+        self.phase1_question_indexes = []
 
-        return [q1, q2, q3]
+        # on prend au hasard des indexes de questions profs
+        self.phase1_question_indexes = sample(self.get_prof_questions(), 2)
+        # puis on ajoute une question étudiant
+        self.phase1_question_indexes.append(sample(self.get_student_questions(), 1)[0])
+        #on melange les questions pour que celle de l'etudiant ne soit pas toujours a la fin
+        shuffle(self.phase1_question_indexes)
+
+        print
+        print "Selected questions : ", self.phase1_question_indexes
+        print
+
+        # on prend le texte des questions profs que l'on vient d'ajouter
+        for i in self.phase1_question_indexes:
+            lst_txt.append(self.dict_questions[i]['s_text'])
+
+        return lst_txt
+
 
     def get_data_phase3(self, context=None):
         """
@@ -123,23 +135,24 @@ class P3eXBlock(XBlock):
         A handler to validate the phase 1
         """
         print 
-        print "Appel au handler"
+        print "Appel au handler 1"
+        print "data : ", data
 
-        self.r1 = data['r1']
-        self.r2 = data['r2']
-        self.r3 = data['r3']
+        for i in range(3):
+            question_index = self.phase1_question_indexes[i]
+            answer = data[i]['answer']
+            grade = int(data[i]['question_grade'])
 
-        # json_resp = Response.create_json(v_question_id = 1, v_text = data['r1'])
-        # print "json 1 created : ", json_resp
-        # self.responses.append(json_resp)
-        # print "rep added : ", self.responses
-        # json_resp = Response.create_json(v_question_id = 2, v_text = data['r2'])
-        # self.responses.append(json_resp)
-        # json_resp = Response.create_json(v_question_id = 3, v_text = data['r3'])
-        # self.responses.append(json_resp)
+            # Pour ne pas perdre de precision a cause de la moyenne,
+            # on sauvegarde separement le total de evaluations et le nombre d'evaluation 
+            self.dict_questions[question_index]['nb_of_grade']+=1
+            self.dict_questions[question_index]['n_grade'] += grade
+            self.add_answer_to_question(question_index, answer)
 
         self.current_phase = 2
         print "Fin du handler"
+
+        return {'phase_number':self.current_phase, 'content':self.get_current_html()}
 
     @XBlock.json_handler
     def validate_phase2(self, data, suffix=''):
@@ -148,35 +161,13 @@ class P3eXBlock(XBlock):
         """
 
         print
-        print "Debut handler"
-        # a = Answer(p_s_text = data['r'])
-        # q = Question(p_s_text = data['q'], p_lst_clue_answer = [a])
-        # q_json = q.to_json()
-        # print "apres to_json : ", q_json
-        # print "apres from_json : ", Question.from_json(q_json)
-        # print "is equal ? ", Question.from_json(q_json)==q
-        # self.questions.append(q_json)
-
-        a = {
-            'answer_id': -1,
-            'n_writer_id': -1,
-            's_text': data['r'],
-            'n_grade': -1,
-            'nb_of_grade': -1,
-        }
-        q = {
-            'n_question_id': -1,
-            'n_writer_id': -1,
-            's_text': data['q'],
-            'lst_clue_answer': [a],
-            'n_grade': 4,
-            'nb_of_grade': -1,
-            'lst_answer_to_evaluate': [],
-        }
-        self.questions.append(q)
+        print "Appel au handler 2"
+        self.add_question(data['question'], data['answer'])
 
         self.current_phase = 3
         print "Fin handler"
+
+        return {'phase_number':self.current_phase, 'content':self.get_current_html()}
 
     @XBlock.json_handler
     def validate_phase3(self, data, suffix=''):
@@ -188,6 +179,44 @@ class P3eXBlock(XBlock):
 
         self.current_phase = 4
 
+    def add_question(self, p_question_txt, p_answer_txt, p_is_prof=False):
+        self.max_id_question+=1
+        res = {
+            # 'n_question_id': -42, --> pas besoin a cause du self.max_id_question
+            'n_writer_id': -1,
+            'is_prof': p_is_prof,
+            's_text': p_question_txt,
+            'lst_clue_answer': [{
+                'answer_id': -42,
+                'n_writer_id': -1,
+                's_text': p_answer_txt,
+                'n_grade': 0,
+                'nb_of_grade': 0,
+            }],
+            'n_grade': 0,
+            'nb_of_grade': 0,
+            'lst_answer_to_evaluate': [],
+        }
+        # la cle d'un field.Dict passe au format unicode
+        self.dict_questions[unicode(self.max_id_question)] = res
+
+    def add_answer_to_question(self, id_question, p_s_text):
+        self.dict_questions[id_question]['lst_answer_to_evaluate'].append({
+            'answer_id': -42,
+            'n_writer_id': -1,
+            's_text': p_s_text,
+            'n_grade': 0,
+            'nb_of_grade': 0,
+        })
+
+    def get_prof_questions(self):
+        """Return a subset of all questions written by a professor"""
+        return dict(filter(lambda k: k[1]['is_prof']==True, self.dict_questions.items()))
+
+    def get_student_questions(self):
+        """Return the subset of questions written by students"""
+        return dict(filter(lambda k: k[1]['is_prof']==False, self.dict_questions.items()))
+
     # TO-DO: change this to create the scenarios you'd like to see in the
     # workbench while developing your XBlock.
     @staticmethod
@@ -198,3 +227,7 @@ class P3eXBlock(XBlock):
              """<p3exblock/>
              """),
         ]
+
+# d = {11: {'f': 'orange', 'is_prof': 'False'}, 1: {'f': 'pomme', 'is_prof': 'True'}, 3: {'f': 'orange', 'is_prof': 'False'}, 7: {'f': 'bannane', 'is_prof': 'True'}}
+# sous_ensemble =  [v for k,v in d2.iteritems() if v['f']=='orange']
+# dict(filter(lambda k: k[1]['is_prof']=='True', d.items()))
--- p3exblock/static/js/src/p3exblock.js
+++ p3exblock/static/js/src/p3exblock.js
@@ -1,55 +1,64 @@
 /* Javascript for P3eXBlock. */
 function P3eXBlock(runtime, element) {
 
+    // Saving the URL of the handler
     var urlValid1 = runtime.handlerUrl(element, 'validate_phase1');
     var urlValid2 = runtime.handlerUrl(element, 'validate_phase2');
     var urlValid3 = runtime.handlerUrl(element, 'validate_phase3');
 
-    $('#btn_valid1', element).click(function(eventObject) {
-        r1 = $('#r1').val();
-        r2 = $('#r2').val();
-        r3 = $('#r3').val();
+    function change_phase(data) {
+        console.log("data : ", data)
+        // Replacing the html content by the html of the new phase
+        $(".p3exblock_block").replaceWith(data['content']);
+        $("html, body").animate({ scrollTop: 0 }, "slow");
 
-        n1 = $('#note_saver1').val();
-        n2 = $('#note_saver2').val();
-        n3 = $('#note_saver3').val();
+        // Setting the handler to call on #btn_valid click
+        if (data['phase_number'] == 1) {
+            $('#btn_valid', element).click(P3eXBlock.validate_phase1);
+        } else if (data['phase_number'] == 2) {
+            $('#btn_valid', element).click(P3eXBlock.validate_phase2);
+        } else if (data['phase_number'] == 3) {
+            $('#btn_valid', element).click(P3eXBlock.validate_phase3);
+        }
+    }
 
+    P3eXBlock.validate_phase1 = function(eventObject) {
         $.ajax({
             type: "POST",
             url: urlValid1,
-            data: JSON.stringify(
-                {
-                    "r1": $('#r1').val(),
-                    "r2": $('#r2').val(), 
-                    "r3": $('#r3').val()
-                }),
-            success: setTimeout(function(){location.reload()},2000)
+            data: JSON.stringify([
+                // Sending to server a list of JSON object, each contening the answer 
+                // gave by the student and grade related to a question
+                {'answer': $('#r1').val(), 'question_grade': $('#note_saver1').val()},
+                {'answer': $('#r2').val(), 'question_grade': $('#note_saver2').val()},
+                {'answer': $('#r3').val(), 'question_grade': $('#note_saver3').val()}
+            ]),
+            success: change_phase
         });
-    });
+    }
 
-    $('#btn_valid2', element).click(function(eventObject) {
-        // q = $('#q').val();
-        // r = $('#r').val();
-
+    P3eXBlock.validate_phase2 = function(eventObject) {
         $.ajax({
             type: "POST",
             url: urlValid2,
             data: JSON.stringify({
-                "q": $('#q').val(), 
-                "r": $('#r').val()
+                "question": $('#q').val(), 
+                "answer": $('#r').val()
             }),
-            success: setTimeout(function(){location.reload()},2000)
+            success: change_phase
         });
-    });
+    }
 
-    $('#btn_valid3', element).click(function(eventObject) {
+    P3eXBlock.validate_phase3 = function(eventObject) {
 
         $.ajax({
             type: "POST",
             url: urlValid3,
-            success: setTimeout(function(){location.reload()},2000)
+            success: function(){alert("Fini!")}
         });
-    });
+    }
+
+    $('#btn_valid', element).click(P3eXBlock.validate_phase1);
 
     $(function ($) {
         /* Here's where you'd do things on page load. */
